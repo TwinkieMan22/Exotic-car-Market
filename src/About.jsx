5deeae9386f66091ca02d63c77b323ab58604f82
Shayth Masri <shayth.masri0807@gmail.com>
2024-07-09 09:53:14 -0500

    Added about me. Added super awesome exotic cars

--- src/About.jsx
+++ src/About.jsx
@@ -3,19 +3,14 @@ import {Image} from 'react-bootstrap';
 export default function About() {
     return (
     <div style={{backgroundColor:"#93E9BE"}}>
-        <h1>SHAYTHALICIOUS</h1>   
-        <p><i>We Have the Meats!</i> </p>
+        <h1>The Exotic Car Market</h1>   
+        <p><i>We Have the Cars!</i> </p>
         <p><h4>About Us</h4></p>
-        <p><h6>We take pride in our humane gathering of exotic protein. Each animal we 
-          use for our product is hand picked to be of the best quality. The hardest part 
-          about obtaining exotic meats such as Gorilla, Lion or even Tiger was obtaining
-          the meat in a humane way without poaching any of the animals. We have created beautiful 
-          farms for the animals to be raised on. Each animal lives its life to the fullest 
-          being able to eat at any time. All the animals have a lot of space to run around as 
-          if they are in their natural habitat. We are also going to keep on expanding our 
-          farm so we can house elephants. After we have elephants we are also going to add 
-        a big body of water so we can also house exoctic sea creatures such as whales and sharks. 
-        We also have vegetarian and vegan options such as exotic fruit and vegetables.   </h6></p>
-          <Image src="/Gorilla.jpg" width={(600)}/>      
+        <p><h6>We take pride in our ability in finding some of the most unique cars. Some you might not even believe exist. 
+          We want to make a nice place where everyone can buy the car that they desire. We don't want to make it for the famous only
+          we want all sorts of people no matter who you are. We established this company in 1910 when unique cars started to emerge from the shadows. 
+          We used to only sell cars in person but now we have a online front. We made it extremly easy for our users to use the website and to purchase any car that is listed. 
+          We add a new listing about once every two to three weeks.    </h6></p>
+          <Image src='/Cars car.jpg' width={550} />
       </div>);
 }
--- src/Buy.jsx
+++ src/Buy.jsx
@@ -2,24 +2,30 @@ import { useState } from 'react'
 import { Container, Row, Col, Button, Image, Card, Carousel } from 'react-bootstrap';
 export default function Buy () {
 return (
-    <div>
+    <div style={{backgroundColor:"#93E9BE"}}>   
       <h1>Purchase </h1>
       <p><i>All pricing is in USD</i></p>
-      <Image src ="./Koko.jpg" width={400} />
-      <p><h7>This is Koko! You can purchase her meat  for only <h5>98.99$Lb</h5></h7></p>
+      <Image src ="/Banana.jpeg" style={{padding:'50px'}} width={"40%"}  />
+      <p> <h5>7585$</h5><h7>This is the fun Banana car. It is a extremly unique fun car that will 
+        have all the heads turning.  The car is extremly safe for anyone to drive.DISCLAIMER the people in the photo 
+        are not included.  </h7></p>
 
-      <Image src="./Simba.jpg" width={400} />
-      <p><h7>This is Simba and his lost brother Timba. They are lion cubs so they are extra 
-        tender, you can buy one of them for only <h5>200$</h5></h7></p>
+      <Image src="./Ford Model A.jpg" width={"40%"} style={{padding:'50px'}} />
+      <p> <h5>50000$</h5><h7>This is a one of a kind Ford Model A. They made this car through 1927-1931. This car might 
+        not have any of the new safety measures new cars have but it does have style. Its top speed is 65mph with
+        40hp. You can not find one of these cars in such good condition.  </h7></p>
 
         <Image src="./Toucan.jpg" width={400} />
-        <p><h7>This is a magestic Toucan Tweety. Their meat is extremely sweet, costing only, <h5>90$Lb</h5></h7></p>
+        <p><h5>90$Lb</h5></p>
 
-        <Image src="public/" width={400} />
+        <Image src ="./Koko.jpg" width={400} />
+        <p> <h5>98.99$Lb</h5></p>
 
-        <Image src="public/" width={400} />
+        <Image src ="./Koko.jpg" width={400} />
+        <p> <h5>98.99$Lb</h5></p>
 
-        <Image src="public/" width={400} />
+        <Image src ="./Koko.jpg" width={400} />
+        <p> <h5>98.99$Lb</h5></p>
     </div>
   );
 }
